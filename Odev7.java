d52d9329b2aa210ebcf421586b94ba6d5d1a0122
Yunus Kavcuk <65766059+ByKas22@users.noreply.github.com>
2020-05-26 15:16:58 +0300

    Update Odev7.java

--- Odev7.java
+++ Odev7.java
@@ -1,81 +1,82 @@
+/* Türk lirası oalrak girilen paranın parametreli metod kullanarak diğer para birimlerine dönüştüren bir uygulamadır*/
 
 package odev.pkg7;
-
-import java.util.Scanner;
-
+import java.util.Scanner; //Scanner sınıfını kütüphaneye ekledik.
 public class Odev7 
 {
     public static void main(String[] args) 
     {
-        int para;
-        String tür;
-        Scanner girdi = new Scanner(System.in);
-        for (int i = 1; i > 0; i++) 
+        int para; //int türünde para adında değişken oluşturduk.
+        String tür; String veri türünde tür adında değişken oluşturduk.
+        Scanner girdi = new Scanner(System.in); //klavyeden değer almamızı sağlayan sınıfı çağırdık.
+        for (int i = 1; i > 0; i++) //döngü kullanarak tekrar tekrar aynı işlemi yapabildik.
         {  
-        System.out.println("Elinizdeki Türk Lirası Miktarını Giriniz");
-        para =girdi.nextInt();
+        System.out.println("Elinizdeki Türk Lirası Miktarını Giriniz"); //ekranda yazan ifade.
+        para =girdi.nextInt(); //klavyeden değer alabildik.
         System.out.println("Hangi Para Birimine Dönüştürmek İstersiniz(dolar,euro,leva,paund,ruble)");
-        tür = girdi.next();   
-        switch(tür)
+        //ekranda yazan ifade.
+        tür = girdi.next(); //klavyeden değer almamızı sağlar.   
+        switch(tür) //sorgu yapabilmek için bu ifadeyi kullandık.
         {
-            case "dolar":
-                dolar(para);
-            break;
-            case "euro":
-                euro(para);
-            break;
-            case "leva":
-                leva(para);
-            break;
-            case "paund":
-                paund(para);
-            break;
-            case "ruble":
-                ruble(para);
-            break;
-            default:
-                System.out.println("Lütfen Belirtilen Para Brimlerinden Birini Giriniz");    
+            case "dolar": //eğer ki dolar yazarsak. 
+                dolar(para); //dolar metodunu çağırır.
+            break; işlemi bitirir.
+            case "euro": //eğer ki euro yazarsak.
+                euro(para); euro metodunu çağırır.
+            break; işlemi bitirir.
+            case "leva": //eğer ki leva yazarsak.
+                leva(para); leva metodunu çağırır.
+            break; işlemi bitirir.
+            case "paund": //eğer ki paund yazarsak.
+                paund(para); paund metodunu çağırır.
+            break; işlemi bitirir.
+            case "ruble": //eğer ki ruble yazarsak.
+                ruble(para); ruble metodunu çağırır.
+            break; işlemi bitirir.
+            default: //eğer ki hiçbir koşul sağlanmaz ise;
+                System.out.println("Lütfen Belirtilen Para Birimlerinden Birini Giriniz"); //ekrana yazar.    
         }       
         }
      }   
-    static double dolar(int a)
+    static double dolar(int a) //dolar metodu oluşturduk.(parametreli)
     {
-     double b;
-     double dolar=6.82;
-     b=a/dolar;
-     System.out.println(a+"TL = "+b+"Dolar");
-    return b;
+     double b; //double veri türünde b değişkeni oluşturduk.
+     double dolar=6.82; //double veri türünde dolar değişkeni oluşturduk.
+     b=a/dolar; //yapılan işlemi b değişkenine atadık.
+     System.out.println(a+"TL = "+b+"Dolar"); //ekrana yazdırdık.
+    return b; //geri değer döndürdük.
     }
-    static double euro(int a)
+    static double euro(int a) //euro metodu oluşturduk.(parametreli)
     {
-     double b;
-     double euro=7.43;
-     b=a/euro;
-     System.out.println(a+"TL = "+b+"Euro");
-    return b;
+     double b; //double veri türünde b değişkeni oluşturduk.
+     double euro=7.43; //double veri türünde euro değişkeni oluşturduk.
+     b=a/euro; //yapılan işlemi b değişkenine atadık.
+     System.out.println(a+"TL = "+b+"Euro"); //ekrana yazdırdık.
+    return b; //geri değer döndürdük.
     }
-    static double leva(int a)
+    static double leva(int a) //leva metodu oluşturduk.(parametreli)
     {
-     double b;
-     double leva=3.80;
-     b=a/leva;
-     System.out.println(a+"TL = "+b+"Leva");
-    return b;
+     double b; //double veri türünde b değişkeni oluşturduk.
+     double leva=3.80; //double veri türünde leva değişkeni oluşturduk.
+     b=a/leva; //yapılan işlemi b değişkenine atadık.
+     System.out.println(a+"TL = "+b+"Leva"); //ekrana yazdırdık.
+    return b; //geri değer döndürdük.
     }
-    static double paund(int a)
+    static double paund(int a) //paund metodu oluşturduk.(parametreli)
     {
-     double b;
-     double paund=8.31;
-     b=a/paund;
-     System.out.println(a+"TL = "+b+"Paund");
-    return b;
+     double b; //double veri türünde b değişkeni oluşturduk.
+     double paund=8.31; //double veri türünde paund değişkeni oluşturduk.
+     b=a/paund; //yapılan işlemi b değişkenine atadık.
+     System.out.println(a+"TL = "+b+"Paund"); //ekrana yazdırdık.
+    return b; //geri değer döndürdük.
     }
-    static double ruble(int a)
+    static double ruble(int a) //ruble metodu oluşturduk.(parametreli)
     {
-     double b;
-     double ruble=0.95;
-     b=a/ruble;
-     System.out.println(a+"TL = "+b+"Ruble");
-    return b;
+     double b; //double veri türünde b değişkeni oluşturduk.
+     double ruble=0.95; //double veri türünde ruble değişkeni oluşturduk.
+     b=a/ruble; //yapılan işlemi b değişkenine atadık.
+     System.out.println(a+"TL = "+b+"Ruble"); //ekrana yazdırdık.
+    return b; //geri değer döndürdük.
     }
 }
+
